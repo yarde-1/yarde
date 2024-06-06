dee2c55fc88e7e72498fe299547772f891e67c88
yarde-1 <yarden2.friz@gmail.com>
2024-06-05 22:36:07 -0400

    Update script1.js

--- Maxi ev3/scripts/script1.js
+++ Maxi ev3/scripts/script1.js
@@ -12,217 +12,253 @@ class Conductor {
     get getRut() {
         return this._rut;
     }
+
     get getNombre() {
         return this._nombre;
     }
+
     get getApellido() {
         return this._apellido;
     }
+
     get getEdad() {
         return this._edad;
     }
 }
 
-
-class Auto {
-    constructor(patente, marca, modelo, color, año) {
+class Auto{
+    constructor(patente, marca, modelo, color, año){
         this._patente = patente;
         this._marca = marca;
         this._modelo = modelo;
         this._color = color;
         this._año = año
     }
-    get getPatente() {
+    get getPatente(){
         return this._patente;
     }
-    get getMarca() {
+    get getMarca(){
         return this._marca;
     }
-    get getModelo() {
+    get getModelo(){
         return this._modelo;
     }
-    get getColor() {
+    get getColor(){
         return this._color;
     }
-    get getAño() {
+    get getAño(){
         return this._año
     }
 }
 
 class Carrera {
-    constructor(numCarrera, fecha, direccion) {
+    constructor(numCarrera, fecha, direccion){
         this._numCarrera = numCarrera;
         this._fecha = fecha;
         this._direccion = direccion;
         this._participantes = [];
     }
-    get getNumCarrera() {
+    get getNumCarrera(){
         return this._numCarrera;
     }
-    get getFecha() {
+    get getFecha(){
         return this._fecha;
     }
-    get getDireccion() {
+    get getDireccion(){
         return this._direccion;
     }
-    get getParticipantes() {
+    get getParticipantes(){
         return this._participantes;
     }
-    addParticipante(conductor, auto) {
-        this._participantes.push({ conductor, auto });
+    setParticipante(conductor){
+        this._conductor = conductor
+    }
+    setAutos(auto){
+        this._auto = auto
     }
 }
 
 let conductores = [];
 let autos = [];
 let carreras = [];
+let participantes = {};
 
-conductores.push(new Conductor(1 - 1, "Fanshesco", "Virgorini", 45));
-conductores.push(new Conductor(1 - 2, "Carla", "Cristi", 23));
-conductores.push(new Conductor(1 - 3, "Luis", "Martinez", 30));
-conductores.push(new Conductor(1 - 4, "Ana", "Gonzalez", 34));
-conductores.push(new Conductor(1 - 5, "Pedro", "Ramirez", 29));
-conductores.push(new Conductor(1 - 6, "Maria", "Fernandez", 40));
-conductores.push(new Conductor(1 - 7, "Jose", "Lopez", 31));
-conductores.push(new Conductor(1 - 8, "Lucia", "Sanchez", 27));
-conductores.push(new Conductor(1 - 9, "Miguel", "Diaz", 33));
-conductores.push(new Conductor(1 - 10, "Elena", "Perez", 36));
+conductores.push(new Conductor(1-1, "Fanshesco", "Virgorini", 45));
+conductores.push(new Conductor(1-2, "Carla", "Cristi", 23));
+conductores.push(new Conductor(1-3, "Luis", "Martinez", 30));
+conductores.push(new Conductor(1-4, "Ana", "Gonzalez", 34));
+conductores.push(new Conductor(1-5, "Pedro", "Ramirez", 29));
+conductores.push(new Conductor(1-6, "Maria", "Fernandez", 40));
+conductores.push(new Conductor(1-7, "Jose", "Lopez", 31));
+conductores.push(new Conductor(1-8, "Lucia", "Sanchez", 27));
+conductores.push(new Conductor(1-9, "Miguel", "Diaz", 33));
+conductores.push(new Conductor(1-10, "Elena", "Perez", 36));
 
 console.log(conductores);
 
+//validar rut
+function validarRUT(rut) {
+    let regex = /^\d{1,2}\.\d{3}\.\d{3}-[0-9kK]{1}$/;
+    return regex.test(rut);
+}
+
 //Añadir objetos
-let addConductor = function () {
+let addConductor = function() {
     let rt = document.getElementById("con-rut").value;
     let nom = document.getElementById("con-nom").value;
     let ape = document.getElementById("con-ape").value;
-    let edad = parseInt(document.getElementById("con-edad").value);
-    let con = new Conductor(rt, nom, ape, edad);
+    let edad = document.getElementById("con-edad").value;
 
-    conductores.push(con);
-    alert("Conductor añadido");
-    console.log(conductores);
-    // Remover cualquier clase de error existente
-    rt.classList.remove('is-invalid');
-    nom.classList.remove('is-invalid');
-    ape.classList.remove('is-invalid');
-    edad.classList.remove('is-invalid');
+    let isValid = true;
 
-    // Validar RUT en formato chileno
-    if (!validarRutChileno(rt.value.trim())) {
-        rt.classList.add('is-invalid');
-        return;
+    if (!validarRUT(rt)) {
+        document.getElementById("con-rut").classList.add("is-invalid");
+        isValid = false;
+    } else {
+        document.getElementById("con-rut").classList.remove("is-invalid");
     }
 
-    // Validar otros campos
-    if (nom.value.trim() === '') {
-        nom.classList.add('is-invalid');
-        return;
+    if (nom.trim() === '') {
+        document.getElementById("con-nom").classList.add("is-invalid");
+        isValid = false;
+    } else {
+        document.getElementById("con-nom").classList.remove("is-invalid");
     }
 
-    if (ape.value.trim() === '') {
-        ape.classList.add('is-invalid');
-        return;
+    if (ape.trim() === '') {
+        document.getElementById("con-ape").classList.add("is-invalid");
+        isValid = false;
+    } else {
+        document.getElementById("con-ape").classList.remove("is-invalid");
     }
 
-    if (edad.value.trim() === '') {
-        edad.classList.add('is-invalid');
-        return;
+    if (edad.trim() === '' || isNaN(edad) || edad <= 0) {
+        document.getElementById("con-edad").classList.add("is-invalid");
+        isValid = false;
+    } else {
+        document.getElementById("con-edad").classList.remove("is-invalid");
     }
 
+    if (isValid) {
+        let con = new Conductor(rt, nom, ape, parseInt(edad));
+        conductores.push(con);
+        alert("Conductor añadido");
+        console.log(conductores);
+        document.getElementById('conductor-form').reset();
+        document.getElementById("con-rut").classList.remove("is-invalid");
+        document.getElementById("con-nom").classList.remove("is-invalid");
+        document.getElementById("con-ape").classList.remove("is-invalid");
+        document.getElementById("con-edad").classList.remove("is-invalid");
+    }
 }
-    // Función para validar RUT chileno
-    function validarRutChileno(rut) {
-        let regex = /^\d{1,2}\.\d{3}\.\d{3}-[0-9kK]{1}$/;
-        return regex.test(rut);
+
+///////////////////////////
+
+
+
+//////////////////////////
+let addAuto = function(){
+    let pt = document.getElementById("a-pt").value;
+    let mar = document.getElementById("a-mar").value;
+    let mod = document.getElementById("a-mod").value;
+    let color = document.getElementById("a-color").value;
+    let año = parseInt(document.getElementById("a-año").value);
+
+
+    let a = new Auto(pt, mar, mod, color, año);
+
+    autos.push(a);
+    alert("Auto añadido");
+    console.log(autos);
+}
+
+let addCarrera = function(){
+    let numC = document.getElementById("c-numC").value;
+    let fecha = document.getElementById("c-fecha").value;
+    let dirc = document.getElementById("c-dirc").value;
+
+    let carrera = new Carrera(numC, fecha, dirc);
+
+    carreras.push(carrera);
+    alert("Carrera añadida");
+    console.log(carreras);
+}
+
+let addParticipante = function(){
+    let bCar = document.getElementById("c-numC")
+    let car = carreras.find(carre => carre.getNumCarrera === bCar)
+    if(car != undefined){
+        let part = conductores.find(pa => pa.getNumCarrera === rut)
+        p.setParticipante(part)
+
+        participantes.push(p)
+        alert("Participante agregado")
+        console.log(participantes)
+
+        let vehi = autos.find(a => a.getNumCarrera === patente)
+        au.setAutos(vehi)
+
+        autos.push(au)
+        alert("Auto agregado")
+        console.log(autos)
+    }else{
+        alert("no se encontro")
     }
+    
+}
 
+//Busqueda de objetos
 
-
-
-    let addAuto = function () {
-        let pt = document.getElementById("a-pt").value;
-        let mar = document.getElementById("a-mar").value;
-        let mod = document.getElementById("a-mod").value;
-        let color = document.getElementById("a-color").value;
-        let año = parseInt(document.getElementById("a-año").value);
-
-
-        let a = new Auto(pt, mar, mod, color, año);
-
-        autos.push(a);
-        alert("Auto añadido");
-        console.log(autos);
+let findConductor = function() {
+    let rC = document.getElementById("b-rut").value;
+    if (!validarRUT(rC)) {
+        alert("RUT inválido. Por favor, ingrese un RUT válido en formato xx.xxx.xxx-x.");
+        document.getElementById("b-rut").classList.add("is-invalid");
+        return;
+    } else {
+        document.getElementById("b-rut").classList.remove("is-invalid");
     }
-
-    let addCarrera = function () {
-        let numC = document.getElementById("c-numC").value;
-        let fecha = document.getElementById("c-fecha").value;
-        let dirc = document.getElementById("c-dirc").value;
-
-        let carrera = new Carrera(numC, fecha, dirc);
-        let conductorRut = document.getElementsByName("c-rutCon");
-        let autoPatentes = document.getElementsByName("c-auto");
-
-        for (let i = 0; i < conductorRut.length; i++) {
-            let rutCon = conductorRut[i].value;
-            let autoPatente = autoPatentes[i].value;
-
-            let conductor = conductores.find(con => con.getRut === rutCon);
-            let auto = autos.find(aut => aut.getPatente === autoPatente);
-
-            if (conductor && auto) {
-                carrera.addParticipante(conductor, auto);
-            }
-        }
-
-        carreras.push(carrera);
-        alert("Carrera añadida");
-        console.log(carreras);
+    
+    let c = conductores.find(con => con.getRut === rC);
+    if (c !== undefined) {
+        alert("Conductor encontrado.");
+        document.getElementById("rC-rutC").innerHTML = "<b>Rut: </b>" + c.getRut;
+        document.getElementById("rC-nombreC").innerHTML = "<b>Nombre: </b>" + c.getNombre + " " + c.getApellido;
+        document.getElementById("rC-edadC").innerHTML = "<b>Edad: </b>" + c.getEdad + " Años.";
+    } else {
+        alert("Conductor no encontrado.");
     }
+}
 
-    //Busqueda de objetos
+let findAuto = function(){
 
-    let findConductor = function () {
-
-        let rC = document.getElementById("b-rut").value;
-        let c = conductores.find(con => con.getRut === rC);
-        if (c != undefined) {
-            alert("Conductor encontrado.");
-            document.getElementById("rC-rutC").innerHTML = "<b>Rut: </b>" + c.getRut;
-            document.getElementById("rC-nombreC").innerHTML = "<b>Nombre: </b>" + c.getNombre + " " + c.getApellido;
-            document.getElementById("rC-edadC").innerHTML = "<b>Edad: </b>" + c.getEdad + " Años.";
-        } else {
-            alert("Conductor no encontrado.");
-        }
+    let pA = document.getElementById("b-pat").value;
+    let a = autos.find(aut => aut.getPatente === pA);
+    if(a != undefined){
+        alert("Auto encontrado.");
+        document.getElementById("rA-patA").innerHTML = "<b>Patente: </b>"+a.getPatente;
+        document.getElementById("rA-marcA").innerHTML = "<b>Marca: </b>"+a.getMarca;
+        document.getElementById("rA-modA").innerHTML = "<b>Modelo: </b>"+a.getModelo;
+        document.getElementById("rA-colorA").innerHTML = "<b>Color: </b>"+a.getColor;
+        document.getElementById("rA-añoA").innerHTML = "<b>Año: </b>"+a.getAño;
+    }else{
+        alert("Auto no encontrado.");
     }
+}
 
-    let findAuto = function () {
+let findCarrera = function(){
 
-        let pA = document.getElementById("b-pat").value;
-        let a = autos.find(aut => aut.getPatente === pA);
-        if (a != undefined) {
-            alert("Auto encontrado.");
-            document.getElementById("rA-patA").innerHTML = "<b>Patente: </b>" + a.getPatente;
-            document.getElementById("rA-marcA").innerHTML = "<b>Marca: </b>" + a.getMarca;
-            document.getElementById("rA-modA").innerHTML = "<b>Modelo: </b>" + a.getModelo;
-            document.getElementById("rA-colorA").innerHTML = "<b>Color: </b>" + a.getColor;
-            document.getElementById("rA-añoA").innerHTML = "<b>Año: </b>" + a.getAño;
-        } else {
-            alert("Auto no encontrado.");
-        }
+    let nC = document.getElementById("b-numC").value;
+    let c = carreras.find(car => car.getNumCarrera === nC);
+    if(c != undefined){
+        alert("Carrera encontrada.");
+        document.getElementById("rC-numCar").innerHTML = "<b>Numero de Carrera: </b>"+c.getNumCarrera;
+        document.getElementById("rC-fechaCar").innerHTML = "<b>Fecha: </b>"+c.getFecha;
+        document.getElementById("rC-dircCar").innerHTML = "<b>Direccion: </b>"+c.getDireccion;
+        document.getElementById("rC-numCar").innerHTML = "<b>NumeroCarrera: </b>"+c.getNumCarrera;
+    }else{
+        alert("Carrera no encontrada.");
     }
+}
 
-    let findCarrera = function () {
-
-        let nC = document.getElementById("b-numC").value;
-        let c = carreras.find(car => car.getNumCarrera === nC);
-        if (c != undefined) {
-            alert("Auto encontrado.");
-            document.getElementById("rC-numCar").innerHTML = "<b>Numero de Carrera: </b>" + c.getNumCarrera;
-            document.getElementById("rC-fechaCar").innerHTML = "<b>Fecha: </b>" + c.getFecha;
-            document.getElementById("rC-dircCar").innerHTML = "<b>Direccion: </b>" + c.getDireccion;
-            document.getElementById("rC-numCar").innerHTML = "<b>NumeroCarrera: </b>" + c.getNumCarrera;
-        } else {
-            alert("Carrera no encontrada.");
-        }
-    }
+//validacion
